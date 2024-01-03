38782051f85a41c78aeffcdc3758209115cd7b67
Daniel <daniel.munoz-martin@energie360.ch>
2024-01-03 21:45:30 +0100

    add todo

--- routes/downloads/index.ts
+++ routes/downloads/index.ts
@@ -9,5 +9,6 @@ export default defineCachedEventHandler(async (event) => {
         metadata
     ] = await fetchFromCollection<DownloadDoc, DownloadMetadata>(AvailableMockData.Downloads);
 
+    // TODO: Pagination
     return { data, metadata }
 }, { swr: false })
