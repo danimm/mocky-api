e6b94465d47016413f2626c210e14757f47ae858
Daniel <daniel.munoz-martin@energie360.ch>
2024-01-05 17:50:01 +0100

    remove cached event and format

--- routes/downloads/index.ts
+++ routes/downloads/index.ts
@@ -1,20 +1,20 @@
 import { AvailableMockData } from "../../types/availableMockData";
 
-export default defineCachedEventHandler(async (event) => {
+export default defineEventHandler(async (event) => {
   const { fetchFromCollection } = useDB(event);
 
     const { page = '' } = getQuery<{ page: string }>(event)
 
-    const [data, metadata] =
-        await fetchFromCollection(AvailableMockData.Downloads, {
-            startAfter: page, perPage: 7,
-        })
+    const [data, metadata] = await fetchFromCollection(AvailableMockData.Downloads, {
+        startAfter: page,
+        perPage: 7,
+    })
 
     if (!data) return createError({
         message: 'No data found in the database or error fetching the data',
         statusCode: 404,
     })
 
-    // TODO: Update return format based on the current API
+
     return { data, metadata }
-}, { swr: false })
+})
