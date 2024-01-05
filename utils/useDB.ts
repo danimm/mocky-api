6d12f5128b6264f66c4c5b77f94afb983d68c636
Daniel <daniel.munoz-martin@energie360.ch>
2024-01-05 17:15:53 +0100

    refactor typing

--- utils/useDB.ts
+++ utils/useDB.ts
@@ -1,20 +1,17 @@
 import {
     getFirestore,
-    getDocs,
-    getDoc,
-    query,
-    where,
+    getDocs, getDoc, query, where,
+    getCountFromServer, documentId, DocumentSnapshot,
     DocumentData,
-    getCountFromServer,
-    documentId,
-    DocumentSnapshot,
 } from "@firebase/firestore";
+
+import { type FetchFromCollectionOptions } from "../types/common/queryOptions";
+import { type DataResponse } from "../types/common/response";
+import { type EventHandlerRequest, H3Event } from "h3";
 
 import { app } from "../firebase";
 import { AvailableMockData, MockDataMap } from "../types/availableMockData";
-import { FetchFromCollectionOptions } from "../types/common/queryOptions";
 import { useQueryPagination } from "./useQueryPagination";
-import { EventHandlerRequest, H3Event } from "h3";
 import { usePaginator } from "./usePaginator";
 
 export function useDB(event?:  H3Event<EventHandlerRequest>) {
@@ -25,72 +22,65 @@ export function useDB(event?:  H3Event<EventHandlerRequest>) {
         (collectionName: MockDataKey, options?: FetchFromCollectionOptions<ResultType>): Promise<[data: ResultType[] | null, metadata: MetadataType | null]>
     async function fetchFromCollection
     <MockDataKey extends AvailableMockData, ResultType = MockDataMap[MockDataKey][0], MetadataType = MockDataMap[MockDataKey][1]>
-        (collectionName: MockDataKey, options: FetchFromCollectionOptions<ResultType> = {}): Promise<[data: ResultType[] | null, metadata: MetadataType | null]> {
+        (collectionName: MockDataKey, options: FetchFromCollectionOptions<ResultType> = {}): Promise<[data: ResultType[] | null, metadata: MetadataType | null]>
+    {
+        const {
+            useCollectionRef,
+            useDocumentRef,
+            getDocument,
+            getDocsData,
+            getDocData,
+        } = useFirestore<ResultType>(db, collectionName)
 
-            const {
-                useCollectionRef,
-                useDocumentRef,
-                getDocument,
-                getDocsData,
-                getDocData,
-            } = useFirestore<ResultType>(db, collectionName)
-
-            // Prepare the references
-            const collectionRef = useCollectionRef()
-            const metadataQuery = useDocumentRef<MetadataType>('metadata')
-            let lastDocument: null | DocumentSnapshot<ResultType, DocumentData>  = null
+        // Prepare the references
+        const collectionRef = useCollectionRef()
+        const metadataQuery = useDocumentRef<MetadataType>('metadata')
+        let lastDocument: null | DocumentSnapshot<ResultType, DocumentData>  = null
 
 
-            // Queries and pagination
-            if (options.startAfter) lastDocument = await getDocument(options.startAfter)
+        // Queries and pagination
+        if (options.startAfter) lastDocument = await getDocument(options.startAfter)
 
-            const allDocumentsQuery = query(
-                collectionRef,
-                // TODO: remove this for generic use
-                where('created_at', "!=", ''),
-                ...useQueryPagination<ResultType>({...options, startAfter: lastDocument })
-            )
+        const allDocumentsQuery = query(
+            collectionRef,
+            // TODO: remove this for generic use
+            where('created_at', "!=", ''),
+            ...useQueryPagination<ResultType>({...options, startAfter: lastDocument })
+        )
 
-            const countDocumentsQuery = query(collectionRef, where(documentId(), "!=", 'metadata'))
+        const countDocumentsQuery = query(collectionRef, where(documentId(), "!=", 'metadata'))
 
-            // Fetch the data
-            const [
-                dataPromise,
-                metadataPromise,
-                totalCountPromise
-            ] = await Promise.allSettled([
-                getDocs(allDocumentsQuery),
-                getDoc(metadataQuery),
-                getCountFromServer(countDocumentsQuery)
-            ])
+        // Fetch the data
+        const response = await Promise.allSettled([
+            getDocs(allDocumentsQuery),
+            getDoc(metadataQuery),
+            getCountFromServer(countDocumentsQuery)
+        ])
 
-            const lastPosition = dataPromise.status === 'fulfilled' && dataPromise.value.docs.at(-1)
-            const totalCount = totalCountPromise.status === 'fulfilled' ? totalCountPromise.value.data().count : 0
+        if (response.some(({ status }) => status === 'rejected')) return [null, null]
 
-            // Prepare the data
-            const data = dataPromise.status === 'fulfilled' && !dataPromise.value?.empty
-                ? getDocsData(dataPromise.value)
-                : null
+        const [dataPromise, metadataPromise, totalCountPromise] = response as DataResponse<ResultType, MetadataType>
 
-                // TODO: Use & type transform
-                // getDocsData(dataPromise.value, options.transform)
+        // Prepare the data
+        const lastPosition = dataPromise.value.docs.at(-1)
+        const totalCount = totalCountPromise.value.data().count || 0
+        const data = !dataPromise.value?.empty ? getDocsData(dataPromise.value) : null
+        // TODO: Try to implement & type transform callback
+        // getDocsData(dataPromise.value, options.transform)
 
-            const metadata =  metadataPromise.status === 'fulfilled' &&  metadataPromise.value.exists()
-                ? {
-                    ...getDocData(metadataPromise.value),
-                    ...usePaginator({
-                        url: event ? getRequestURL(event) : new URL(''),
-                        lastPositionId: lastPosition.ref.id,
-                        options,
-                        totalCount
-                    })
-                }
-                : null
+        const metadata =  metadataPromise.value.exists()
+            ? {
+                ...getDocData(metadataPromise.value),
+                ...usePaginator({
+                    url: event ? getRequestURL(event) : new URL(''),
+                    lastPositionId: lastPosition.ref.id,
+                    options,
+                    totalCount
+                })
+            }
+            : null
 
-            return [
-                data,
-                metadata,
-            ];
+        return [data, metadata];
     }
 
     return { fetchFromCollection, db }
