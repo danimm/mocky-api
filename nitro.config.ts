118d7d2cad55b361d75ca77efb674d496eead9d1
Daniel <daniel.munoz-martin@energie360.ch>
2024-01-08 10:13:16 +0100

    remove storage layer

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,10 +1,4 @@
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
-    preset: "deno-deploy",
-    storage: {
-        db: {
-            driver: 'fs',
-            base: './data/db'
-        }
-    }
+    preset: "deno-deploy"
 });
