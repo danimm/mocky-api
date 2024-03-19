573d3e22ccf512be6cee161315d099dbeb47a57c
Daniel <daniel.munoz-martin@energie360.ch>
2024-03-19 13:13:08 +0100

    Preparing mocky's broken link shapes

---
+++ mockData/storybook/index.ts
@@ -0,0 +1,125 @@
+/**
+ * https://run.mocky.io/v3/fc382300-2377-4c7c-89ef-f1c1cf89b7b3
+ * Used in the following stories:
+ *
+ * intro-with-map
+ *
+ */
+const mockData1 = []
+
+/**
+ * https://run.mocky.io/v3/ea8b434f-294f-4008-9634-51c241fdca5b
+ * Used in the following stories:
+ *
+ * wizard-meter-reading-result
+ * wizard-reloc-result
+ *
+ */
+const mockData2 = []
+
+/**
+ * https://run.mocky.io/v3/f3226260-2e70-426f-a167-71e81220c02f
+ * Used in the following stories:
+ *
+ * wizard-reloc-tenant-move-out
+ * wizard-reloc-previous-owner
+ * wizard-reloc-next-owner
+ * wizard-reloc-contact-details
+ * wizard-reloc-new-address-existing
+ * wizard-reloc-new-address-leaving-customer
+ * wizard-reloc-new-address-new-customer
+ *
+ */
+const mockData3 = []
+
+/**
+ * https://run.mocky.io/v3/b759b63a-4c51-4cef-97f1-f628c446a84a
+ * Used in the following stories:
+ *
+ * wizard-price-result
+ *
+ */
+const mockData4 = []
+
+/**
+ * https://run.mocky.io/v3/96b0df81-78de-45b2-a183-5000debd99d0
+ * Used in the following stories:
+ *
+ * character-card-carousel
+ * character-card
+ *
+ */
+const mockData5 = []
+
+/**
+ * https://run.mocky.io/v3/1d506fa8-25d1-4dc5-a79c-b10f79777a78
+ * Used in the following stories:
+ *
+ * form-address-input-field
+ * wizard-meter-reading-check
+ *
+ */
+const mockData6 = []
+
+/**
+ * https://run.mocky.io/v3/bbab1eb5-7c04-4ac7-9e44-eb7ea1170c11
+ * Used in the following stories:
+ *
+ * feedback
+ *
+ */
+const mockData7 = []
+
+/**
+ * https://run.mocky.io/v3/cb93dfb0-27f6-41f8-85b8-ae750905d383
+ * Used in the following stories:
+ *
+ * survey
+ *
+ */
+const mockData8 = []
+
+/**
+ * https://run.mocky.io/v3/f5edd634-b7b3-4dda-a80b-49c284e26370
+ * Used in the following stories:
+ *
+ * form-address-input-field
+ *
+ */
+const mockData9 = []
+
+/**
+ * https://run.mocky.io/v3/c9001fdc-753e-40f4-87ce-de98291f2806
+ * Used in the following stories:
+ *
+ * wizard-pellets-shop-intro
+ *
+ */
+const mockData10 = []
+
+/**
+ * https://run.mocky.io/v3/4da2be7c-8d90-4116-aff9-9c0051d066b4
+ * Used in the following stories:
+ *
+ * wizard-pellets-shop-intro
+ *
+ */
+const mockData11 = []
+
+/**
+ * https://run.mocky.io/v3/8a188ad3-2e70-4eab-b6cf-8071980e1b8f
+ * Used in the following stories:
+ *
+ * wizard-pellets-shop-intro
+ *
+ */
+const mockData12 = []
+
+/**
+ * https://run.mocky.io/v3/22479c96-a4ef-4020-a4e1-200a77a569d3
+ * Used in the following stories:
+ *
+ * wizard-pellets-shop-intro
+ *
+ */
+const mockData13 = []
