69ed40d1ee15894dfc7665845f7b6610e9a4d8a9
Daniel <daniel.munoz-martin@energie360.ch>
2024-03-21 14:34:44 +0100

    update mock daten

--- mockData/storybook/index.ts
+++ mockData/storybook/index.ts
@@ -1,11 +1,13 @@
-/**
+/** ✅
  * https://run.mocky.io/v3/fc382300-2377-4c7c-89ef-f1c1cf89b7b3
  * Used in the following stories:
  *
  * intro-with-map
  *
  */
-const mockData1 = []
+const mockData1 = {
+    description: 'Praeterea iter est quasdam res quas ex communi. Quisque ut dolor gravida, placerat libero vel, euismod. Ut enim ad minim veniam, quis nostrud exercitation.'
+}
 
 /**
  * https://run.mocky.io/v3/ea8b434f-294f-4008-9634-51c241fdca5b
@@ -15,7 +17,33 @@ const mockData1 = []
  * wizard-reloc-result
  *
  */
-const mockData2 = []
+const mockData2 = {
+    update_url: 'https://run.mocky.io/v3/56e387bb-a533-4c95-834c-a687558e2a80',
+    form: {
+        fields: [
+            { id: 'comment', value: '', messages: {}, required: false },
+            { id: 'user_wishes_reply', value: false, messages: {}, required: false },
+            { id: 'email', value: '', messages: {}, required: false },
+            { id: 'phone', value: '', messages: {}, required: false }
+        ],
+        url: '',
+        errors: false,
+        messages: {}
+    }
+}
+
+/**
+ * https://run.mocky.io/v3/56e387bb-a533-4c95-834c-a687558e2a80
+ * Used in the following stories:
+ *
+ * wizard-meter-reading-result
+ * wizard-reloc-result
+ *
+ */
+const mockData3 = {
+    title: 'Vielen Dank für Ihre Rückmeldung!',
+    description: 'Ihr Beitrag hilft uns, unsere Lösungen noch besser an den Bedürfnissen unserer Kunden auszurichten.'
+}
 
 /**
  * https://run.mocky.io/v3/f3226260-2e70-426f-a167-71e81220c02f
@@ -30,7 +58,11 @@ const mockData2 = []
  * wizard-reloc-new-address-new-customer
  *
  */
-const mockData3 = []
+const mockData4 = {
+    results: [
+        { city: 'Zürich', postal_code: 8000 }
+    ]
+}
 
 /**
  * https://run.mocky.io/v3/b759b63a-4c51-4cef-97f1-f628c446a84a
@@ -39,9 +71,11 @@ const mockData3 = []
  * wizard-price-result
  *
  */
-const mockData4 = []
+const mockData5 = {
+    result_text: 'Der Richtpreis als PDF-Datei wurde an max.mustermann@gmail.com gesendet.'
+}
 
-/**
+/** ✅
  * https://run.mocky.io/v3/96b0df81-78de-45b2-a183-5000debd99d0
  * Used in the following stories:
  *
@@ -49,9 +83,14 @@ const mockData4 = []
  * character-card
  *
  */
-const mockData5 = []
+const mockData6 = {
+    current_biogas_amount: 30.0,
+    new_biogas_amount: 55.0,
+    price: 8.2,
+    price_with_vat: 8.83
+}
 
-/**
+/** ✅
  * https://run.mocky.io/v3/1d506fa8-25d1-4dc5-a79c-b10f79777a78
  * Used in the following stories:
  *
@@ -59,7 +98,79 @@ const mockData5 = []
  * wizard-meter-reading-check
  *
  */
-const mockData6 = []
+const mockData7 = {
+    results: [
+        { city: 'Oberengstringen', postal_code: 8102},
+        { city: 'Unterengstringen', postal_code: 8103},
+        { city: 'Weiningen ZH', postal_code: 8104},
+        { city: 'Regensdorf', postal_code: 8105},
+        { city: 'Watt', postal_code: 8105},
+        { city: 'Adlikon b. Regensdorf', postal_code: 8106},
+        { city: 'Buchs ZH', postal_code: 8107},
+        { city: 'Dällikon', postal_code: 8108},
+        { city: 'Kloster Fahr', postal_code: 8109},
+        { city: 'Otelfingen', postal_code: 8112},
+        { city: 'Boppelsen', postal_code: 8113},
+        { city: 'Dänikon ZH', postal_code: 8114},
+        { city: 'Hüttikon', postal_code: 8115},
+        { city: 'Fällanden', postal_code: 8117},
+        { city: 'Pfaffhausen', postal_code: 8118},
+        { city: 'Benglen', postal_code: 8121},
+        { city: 'Binz', postal_code: 8122},
+        { city: 'Ebmatingen', postal_code: 8123},
+        { city: 'Maur', postal_code: 8124},
+        { city: 'Zollikerberg', postal_code: 8125},
+        { city: 'Zumikon', postal_code: 8126},
+        { city: 'Forch', postal_code: 8127},
+        { city: 'Zumikon Yves Rocher SA', postal_code: 8130},
+        { city: 'Egg b. Zürich', postal_code: 8132},
+        { city: 'Hinteregg', postal_code: 8132},
+        { city: 'Esslingen', postal_code: 8133},
+        { city: 'Adliswil', postal_code: 8134},
+        { city: 'Adliswil 1', postal_code: 8134},
+        { city: 'Langnau am Albis', postal_code: 8135},
+        { city: 'Sihlbrugg Station', postal_code: 8135},
+        { city: 'Sihlwald', postal_code: 8135},
+        { city: 'Gattikon', postal_code: 8136},
+        { city: 'Uitikon Waldegg', postal_code: 8142},
+        { city: 'Stallikon', postal_code: 8143},
+        { city: 'Uetliberg', postal_code: 8143},
+        { city: 'Opfikon', postal_code: 8152},
+        { city: 'Glattbrugg', postal_code: 8152},
+        { city: 'Glattpark (Opfikon)', postal_code: 8152},
+        { city: 'Rümlang', postal_code: 8153},
+        { city: 'Oberglatt ZH', postal_code: 8154},
+        { city: 'Niederhasli', postal_code: 8155},
+        { city: 'Nassenwil', postal_code: 8155},
+        { city: 'Oberhasli', postal_code: 8156},
+        { city: 'Dielsdorf', postal_code: 8157},
+        { city: 'Regensberg', postal_code: 8158},
+        { city: 'Steinmaur', postal_code: 8162},
+        { city: 'Sünikon', postal_code: 8162},
+        { city: 'Bachs', postal_code: 8164},
+        { city: 'Oberweningen', postal_code: 8165},
+        { city: 'Schleinikon', postal_code: 8165},
+        { city: 'Schöfflisdorf', postal_code: 8165},
+        { city: 'Niederweningen', postal_code: 8166},
+        { city: 'Niederglatt ZH', postal_code: 8172},
+        { city: 'Neerach', postal_code: 8173},
+        { city: 'Stadel b. Niederglatt', postal_code: 8174},
+        { city: 'Windlach', postal_code: 8175},
+        { city: 'Bülach', postal_code: 8180},
+        { city: 'Höri', postal_code: 8181},
+        { city: 'Hochfelden', postal_code: 8182},
+        { city: 'Bachenbülach', postal_code: 8184},
+        { city: 'Winkel', postal_code: 8185},
+        { city: 'Weiach', postal_code: 8187},
+        { city: 'Glattfelden', postal_code: 8192},
+        { city: 'Zweidlen', postal_code: 8192},
+        { city: 'Eglisau', postal_code: 8193},
+        { city: 'Hüntwangen', postal_code: 8194},
+        { city: 'Wasterkingen', postal_code: 8195},
+        { city: 'Wil ZH', postal_code: 8196},
+        { city: 'Rafz', postal_code: 8197}
+    ]
+}
 
 /**
  * https://run.mocky.io/v3/bbab1eb5-7c04-4ac7-9e44-eb7ea1170c11
@@ -68,7 +179,45 @@ const mockData6 = []
  * feedback
  *
  */
-const mockData7 = []
+const mockData8 = {
+    update_url: '/feedbacks/32425ffa-e6de-462c-af90-efadaa008cab/',
+    form: {
+        fields: [
+            {
+                id: 'comment',
+                value: '',
+                messages: {},
+                required: true
+            },
+            {
+                id: 'user_wishes_reply',
+                value: false,
+                messages: {},
+                required: false
+            },
+            {
+                id: 'email',
+                value: '',
+                messages: {},
+                required: false
+            },
+            {
+                id: 'phone',
+                value: '',
+                messages: {},
+                required: false
+            }
+        ],
+        url: '',
+        errors: false,
+        messages: {}
+    }
+}
+
+// {
+//     'title': 'Vielen Dank f\u00fcr Ihre R\u00fcckmeldung!',
+//     'description': 'Ihr Beitrag hilft uns, unsere L\u00f6sungen noch besser an den Bed\u00fcrfnissen unserer Kunden auszurichten.'
+// }
 
 /**
  * https://run.mocky.io/v3/cb93dfb0-27f6-41f8-85b8-ae750905d383
@@ -77,7 +226,23 @@ const mockData7 = []
  * survey
  *
  */
-const mockData8 = []
+// const request = {
+//     is_b2c: 'true',
+//     new_biogas_amount: '30',
+//     old_biogas_amount: '35',
+//     choices: [
+//         { label: 'Fehlende Anreize', value: 'choice_3' },
+//         { label: 'Zu teuer', value: 'choice_1' },
+//         { label: 'Bevorzuge anderes für Umweltschutz', value: 'choice_4' },
+//         { label: 'Biogas überzeugt mich nicht', value: 'choice_2' },
+//         { label: 'Andere Gründe', value: 'choice_5' }
+//     ],
+//     answer: 'choice_2'
+// }
+const mockData9 = {
+    title: 'Vielen Dank für Ihre Rückmeldung!',
+    description: 'Ihr Beitrag hilft uns, unsere Lösungen noch besser an den Bedürfnissen unserer Kunden auszurichten.'
+}
 
 /**
  * https://run.mocky.io/v3/f5edd634-b7b3-4dda-a80b-49c284e26370
@@ -86,25 +251,63 @@ const mockData8 = []
  * form-address-input-field
  *
  */
-const mockData9 = []
+const mockData10 = {
+    results: [
+        { city: 'Zürich', postal_code: 8000 }
+    ]
+}
 
-/**
+/** ✅
  * https://run.mocky.io/v3/c9001fdc-753e-40f4-87ce-de98291f2806
  * Used in the following stories:
  *
  * wizard-pellets-shop-intro
  *
  */
-const mockData10 = []
+const mockData11 = {
+    products: [
+        {
+            id: 'swiss',
+            image_id: 'swiss_pellets',
+            certificates: [
+                'en_plus',
+                'member_id'
+            ],
+            title: 'Holzpellets Schweizer Holz',
+            description: 'Holz aus der Schweiz, höchster Qualitätsstandard ENplus® A1 für eine hohe Heizleistung und saubere Verbrennung.',
+            price: 'CHF 342.05',
+            price_description: 'Pro Tonne, inkl. MWST und Lieferung. \\r\\nCHF 326.88 exkl. MWST',
+            price_discount: 'statt CHF 352.05'
+        },
+        {
+            id: 'dach',
+            image_id: 'pellets',
+            certificates: [
+                'en_plus'
+            ],
+            title: 'Holzpellets',
+            description: 'Holz aus der Deutschland, Österreich und der Schweiz, höchster Qualitätsstandard ENplus® A1 für eine hohe Heizleistung und saubere Verbrennung.',
+            price: 'CHF 337.05',
+            price_description: 'Pro Tonne, inkl. MWST und Lieferung. \\r\\nCHF 322.24 exkl. MWST',
+            price_discount: 'statt CHF 347.05'
+        }
+    ]
+}
 
-/**
+/** ✅
  * https://run.mocky.io/v3/4da2be7c-8d90-4116-aff9-9c0051d066b4
  * Used in the following stories:
  *
  * wizard-pellets-shop-intro
  *
  */
-const mockData11 = []
+const mockData12 = {
+    message: {
+        title: 'O.Bise - Unser Partner in der West-Schweiz',
+        description: "In der Westschweiz beliefert Sie unsere regionale Partnerfirma O.Bise SA zuverlässig und schnell mit Holzpellets. Bestellungen und Offerten unter <a href='https://www.obise-pellets.ch/'>www.obise-pellets.ch/</a>"
+    },
+    status_code: 404
+}
 
 /**
  * https://run.mocky.io/v3/8a188ad3-2e70-4eab-b6cf-8071980e1b8f
@@ -113,13 +316,68 @@ const mockData11 = []
  * wizard-pellets-shop-intro
  *
  */
-const mockData12 = []
+const mockData13 = {
+    message: {
+        title: 'Abfrage-Limit erreicht',
+        description: 'Sie haben die maximale Anzahl an Online-Preisabfragen an einem Tag erreicht. Bitte kontaktieren Sie uns für weitere Informationen direkt.'
+    }
+}
 
-/**
+/** ✅
  * https://run.mocky.io/v3/22479c96-a4ef-4020-a4e1-200a77a569d3
  * Used in the following stories:
  *
  * wizard-pellets-shop-intro
  *
  */
-const mockData13 = []
+const mockData14 = {
+    products: [
+        {
+            id: 'swiss',
+            image_id: 'swiss_pellets',
+            certificates: [
+                'en_plus',
+                'member_id'
+            ],
+            title: 'Holzpellets Schweizer Holz',
+            description: 'Holz aus der Schweiz, höchster Qualitätsstandard ENplus® A1 für eine hohe Heizleistung und saubere Verbrennung.',
+            price: 'CHF 342.05',
+            price_description: 'Pro Tonne, inkl. MWST und Lieferung. \\r\\nCHF 326.88 exkl. MWST',
+            price_discount: 'statt CHF 352.05'
+        }
+    ]
+}
+
+/** ✅
+ * Used in the following stories:
+ *
+ * Services
+ *
+ */
+
+const mockDataServices = {
+    title: 'Monatliche Kosten KIBAG Management AG',
+    abo_cost_title: 'Abo-Kosten',
+    amount_title: 'Betrag',
+    services_title: 'Services',
+    abos: [
+        {
+            title: 'Abo «start» CHF 7.90 x 23 Ladeparkplätze',
+            price_display: 'CHF 181.70'
+        }
+    ],
+    services: [
+        {
+            title: '24/7 Hotline pauschal für die gesamte Liegenschaft',
+            price_display: 'CHF 23.00'
+        },
+        {
+            title: 'Öffentlich zugänglich CHF 2.35 x 23 Ladeparkplätze',
+            price_display: 'CHF 54.05'
+        }
+    ],
+    total: {
+        title: 'Total',
+        price_display: 'CHF 258.75'
+    }
+}
